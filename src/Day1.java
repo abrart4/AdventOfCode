63f6879a6ca95a9b51073265aef81e5c44cf9a78
abrart4 <abrart4@nycstudents.net>
2025-12-10 22:28:35 -0500

    Do part 2

--- src/Day1.java
+++ src/Day1.java
@@ -13,17 +13,26 @@ public class Day1 {
             String directionString = line.substring(0, 1);
             int direction = directionString.equals("L") ? -1 : 1;
             String numberString = line.substring(1);
-            int number = (Integer.parseInt(numberString) * direction) % 100;
-            current += number;
-            if (current < 0) {
-                current = 100 + current;
+            int number = Integer.parseInt(numberString);
+            for (int i = 0; i < number; i ++) {
+                if (direction == 1) {
+                    current ++;
+                }
+                else {
+                    current --;
+                }
+                if (current % 100 == 0) {
+                    amountOfZero ++;
+
+                }
             }
-            if (current > 99) {
+            if (current < 0) {
+                current = 100 + (current % 100);
+            }
+            else if (current > 99) {
                 current = current % 100;
             }
-            if (current == 0) {
-                amountOfZero ++;
-            }
+            current = current % 100;
         }
         System.out.println(amountOfZero);
 
