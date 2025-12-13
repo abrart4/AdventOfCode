0b9cc53f69f7e26e0e7fea6f7dcfbc94ebab2b7e
abrart4 <abrart4@nycstudents.net>
2025-12-13 16:03:35 -0500

    Finish day 2 part 1

--- src/Day2.java
+++ src/Day2.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 
 public class Day2 {
     public static void main(String[] args) throws Exception {
-        part();
+        part1();
     }
 
-    private static void part() throws Exception {
+    private static void part1() throws Exception {
         long sum = 0;
 
         Scanner input = new Scanner(new File("my day 2 input.txt"));
@@ -25,7 +25,6 @@ public class Day2 {
             long max = Long.parseLong(maxString);
             for (long id = min; id <= max; id ++) {
                 if (isInvalid(id)) {
-                    System.out.println("id " + id + " invalid");
                     sum += id;
                 }
             }
@@ -39,27 +38,31 @@ public class Day2 {
         for (int i = 0; i < digitStrings.length; i ++) {
             digits[i] = Integer.parseInt(digitStrings[i]);
         }
-        Map<Integer, Integer> lastSeen = new HashMap<>();
-        for (int i = 0; i < digits.length; i ++) {
-            int digit = digits[i];
-            if (lastSeen.containsKey(digit)) {
-                int seenOn = lastSeen.get(digit);
-                int difference = i - seenOn;
+        Map<Integer, Integer> lastSeenMap = new HashMap<>();
+        for (int i = 0; i < digits.length; i ++) { // i = 4
+            int digit = digits[i]; // 1
+            if (lastSeenMap.containsKey(digit)) { // true
+                // if (i * 2 >= digits.length) return false;
                 boolean allEqual = true;
-                if (difference + i <= digits.length) {
-                    for (int j = 0; j < difference; j++) {
-                        if (digits[j] != digits[i + j]) {
-                            allEqual = false;
-                        }
-                    }
-                    if (allEqual) {
-
-                        return true;
+                int lastAddition = 0;
+                for (int j = 0; j < i; j ++) { // 0 1 2 3
+                    int fromBeginning = digits[j]; // 1 2 3 4
+                    lastAddition = j + i;
+                    if (lastAddition >= digits.length) return false;
+                    int fromNow = digits[lastAddition]; // 5 6 7 8
+                    if (fromBeginning != fromNow) {
+                        allEqual = false;
                     }
                 }
+
+                if (allEqual && !(lastAddition < digits.length - 1)) {
+                    return true;
+                }
             }
-            lastSeen.put(digit, i);
+            lastSeenMap.put(digit, i);
         }
         return false;
+        // ID:    12341234
+        // Index: 01234567
     }
 }
