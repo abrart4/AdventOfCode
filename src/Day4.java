af76f911bf14a8a26df83322ab2f18a5015f90c7
abrart4 <abrart4@nycstudents.net>
2025-12-14 18:27:16 -0500

    Day 4 part 2

--- src/Day4.java
+++ src/Day4.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Day4 {
     public static void main(String[] args) throws Exception {
-        part1();
+        part2();
     }
 
     private static void part1() throws Exception {
@@ -79,5 +79,85 @@ public class Day4 {
             }
         }
         System.out.println("Day 4 part 1: " + amountOfValid);
+    }
+
+    private static void part2() throws Exception {
+        // sorry for everything i've done
+        ArrayList<String[]> everything = new ArrayList<>();
+        Scanner input = new Scanner(new File("my day 4 input.txt"));
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            everything.add(line.split(""));
+        }
+        int everythingSize = everything.size();
+        int amountOfValid = 0;
+        int amountOfValidThisTime = 1; // not actually true but do it to enter the while loop
+        while (amountOfValidThisTime > 0) {
+            amountOfValidThisTime = 0;
+
+            for (int i = 0; i < everythingSize; i++) {
+                String[] line = everything.get(i);
+                int lineLength = line.length;
+                for (int j = 0; j < lineLength; j++) {
+                    String chr = line[j];
+                    if (chr.equals("@")) {
+                        String[] allAdjacent = new String[8];
+                        boolean thereIsStuffAbove = i != 0;
+                        boolean thereIsStuffToTheLeft = j != 0;
+                        boolean thereIsStuffToTheRight = j != lineLength - 1;
+                        boolean thereIsStuffBelow = i < everythingSize - 1;
+                        if (thereIsStuffAbove) {
+                            String[] previousLine = everything.get(i - 1);
+                            if (thereIsStuffToTheLeft) {
+                                String northWest = previousLine[j - 1];
+                                allAdjacent[0] = northWest;
+                            }
+                            String north = previousLine[j];
+                            allAdjacent[1] = north;
+                            if (thereIsStuffToTheRight) {
+                                String northEast = previousLine[j + 1];
+                                allAdjacent[2] = northEast;
+                            }
+                        }
+                        if (thereIsStuffToTheLeft) {
+                            String west = line[j - 1];
+                            allAdjacent[3] = west;
+                        }
+                        if (thereIsStuffToTheRight) {
+                            String east = line[j + 1];
+                            allAdjacent[4] = east;
+                        }
+                        if (thereIsStuffBelow) {
+                            String[] nextLine = everything.get(i + 1);
+                            if (thereIsStuffToTheLeft) {
+                                String southWest = nextLine[j - 1];
+                                allAdjacent[5] = southWest;
+                            }
+                            String south = nextLine[j];
+                            allAdjacent[6] = south;
+                            if (thereIsStuffToTheRight) {
+                                String southEast = nextLine[j + 1];
+                                allAdjacent[7] = southEast;
+                            }
+                        }
+                        int amountOfAdjacentRolls = 0;
+
+                        for (int k = 0; k < 8; k++) {
+                            String adjacent = allAdjacent[k];
+                            if ("@".equals(adjacent)) {
+                                amountOfAdjacentRolls++;
+                            }
+                        }
+                        if (amountOfAdjacentRolls < 4) {
+                            amountOfValid ++;
+                            amountOfValidThisTime ++;
+                            everything.get(i)[j] = "x";
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Day 4 part 2: " + amountOfValid);
+
     }
 }
