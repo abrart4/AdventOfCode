d969236c1178c610a0064334bc2c683e42742278
abrart4 <abrart4@nycstudents.net>
2025-12-14 03:28:39 -0500

    DO NOT OPEN HAZARDOUS COMMIT

--- src/Day3.java
+++ src/Day3.java
@@ -43,18 +43,52 @@ public class Day3 {
             for (int i = 0; i < digitStrings.length; i ++) {
                 digits[i] = Integer.parseInt(digitStrings[i]);
             }
-            Map<Integer, ArrayList<Integer>> numberToIndices = new HashMap<>();
-            for (int numberToLookFor = 9; numberToLookFor >= 0; numberToLookFor --) {
-                ArrayList<Integer> list = new ArrayList<>();
-                numberToIndices.put(numberToLookFor, list);
-                for (int i = 0; i < digits.length; i ++) {
-                    int digit = digits[i];
-                    if (digit == numberToLookFor) {
-                        list.add(i);
+            long bestJoltageSoFar = -1;
+            int count = 0;
+            for (int a = 0; a < digitStrings.length; a ++) {
+                for (int b = a + 1; b < digitStrings.length; b ++) {
+                    for (int c = b + 1; c < digitStrings.length; c ++) {
+                        for (int d = c + 1; d < digitStrings.length; d ++) {
+                            for (int e = d + 1; e < digitStrings.length; e ++) {
+                                for (int f = e + 1; f < digitStrings.length; f ++) {
+                                    for (int g = f + 1; g < digitStrings.length; g ++) {
+                                        for (int h = g + 1; h < digitStrings.length; h ++) {
+                                            for (int i = h + 1; i < digitStrings.length; i ++) {
+                                                for (int j = i + 1; j < digitStrings.length; j ++) {
+                                                    for (int k = j + 1; k < digitStrings.length; k ++) {
+                                                        for (int l = k + 1; l < digitStrings.length; l ++) {
+                                                            count ++;
+                                                            String comboString =
+                                                                digitStrings[a]
+                                                                    + digitStrings[b]
+                                                                    + digitStrings[c]
+                                                                    + digitStrings[d]
+                                                                    + digitStrings[e]
+                                                                    + digitStrings[f]
+                                                                    + digitStrings[g]
+                                                                + digitStrings[h]
+                                                                + digitStrings[i]
+                                                                + digitStrings[j]
+                                                                + digitStrings[k]
+                                                                + digitStrings[l];
+                                                            long comboLong = Long.parseLong(comboString);
+                                                            if (comboLong > bestJoltageSoFar) {
+                                                                bestJoltageSoFar = comboLong;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
-            System.out.println(numberToIndices);
+            System.out.println(count);
+            totalJoltage += bestJoltageSoFar;
         }
         System.out.println("Day 3 part 2: " + totalJoltage);
     }
