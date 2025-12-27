cfda96455faa0b3738eca84bea02db5d50518683
abrart4 <abrart4@nycstudents.net>
2025-12-26 23:38:07 -0500

    day 7 outline

---
+++ src/Day7.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
+public class Day7 {
+    public static void main(String[] args) throws FileNotFoundException {
+        part1();
+    }
+
+    private static void part1() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("my day 7 input.txt"));
+    }
+
+    private static void part2() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("my day 7 input.txt"));
+    }
+}
